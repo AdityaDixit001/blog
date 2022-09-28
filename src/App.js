43f57002ff5976ae7f9be27a93085f2b128bbbe1
Ajjbawa <104264361+Ajjbawa@users.noreply.github.com>
2022-09-28 20:10:22 +0530

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Home from './pages/Home.js';
-import About from './pages/About.js';
-import Article from './pages/Article.js';
-import ArticlesList from './pages/ArticlesList.js';
 
+import Home from "./pages/Home.js";
+import About from "./pages/About.js";
+import Article from "./pages/Article.js";
+import ArticlesList from "./pages/ArticlesList.js";
+import NotFound from "./pages/NotFound.js";
+
+import NavBar from "./components/NavBar.js";
 
 function App() {
   return (
-    <div className="max-w-screen-md mx-auto pt-20">
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/articles-list" element={<ArticlesList />} />
-      <Route path="/article" element={<Article />} />
-      </Routes>
+      <NavBar />
+      <div className="max-w-screen-md mx-auto pt-20">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/articles-list" element={<ArticlesList />} />
+          <Route path="/article/:name" element={<Article />} />
+          <Route path="*" element={<NotFound/>} />
+        </Routes>
+       
+      </div>
     </BrowserRouter>
-    </div>
   );
 }
 
