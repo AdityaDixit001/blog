e83a646bd8311dbc02cb390143a5061526582fdc
Ajjbawa <104264361+Ajjbawa@users.noreply.github.com>
2022-09-29 10:04:52 +0530

    commit changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,11 +4,11 @@ import React from "react";
 
 const About = () => {
 return (
-<div className="mb-20">
+<div className="mb-20 m-0">
 <h1 className="sm:text-4xl text-2xl font-bold mt-6 text-gray-900">
 About
 </h1>
-<div className="w-40  inline-block ;"><img className="text-align: left" src="./images/ad.jpg"></img></div>
+<div className="w-40  inline-block mt-2 ml-0 ;"><img className="text-align: left" src="./images/ad.jpg"></img></div>
 <p className="mt-4 sm:text-2xl  text-blue-600">Hello my name is aditya dixit. i have done my p.g in mathematics but i have always been passionate about writing. in my childhood i used to participate in various essay writing contest and some debate programmes. i won some of them. In this digital age we cannot write on paper. so fullfill my passion i started this blog channel. so enjoy reading and keep writing</p>
 
 
